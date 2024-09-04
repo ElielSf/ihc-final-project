744a5207476e12aba63f09e8a347934d4c69dc5e
Eliel Souza <elielsf13@gmail.com>
2024-09-04 17:31:04 -0300

    criação do header mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import { MdOutlineLocalGroceryStore } from "react-icons/md";
+import { RxHamburgerMenu } from "react-icons/rx";
 import { CgProfile } from "react-icons/cg";
 import { LuHeart } from "react-icons/lu";
 import { PiMagnifyingGlassBold } from "react-icons/pi";
@@ -7,25 +8,30 @@ import "./css/Header.css"
 
 export default function Header() {
     return (
-        <div className="header_wrapper">
-            <div className="header_logo_wrapper">
-                <img className="header_logo" src="" alt="LOGO" />
+        <div className="header">
+            <div className="header_logo">
+                <img src="./gAtlas-header.svg" alt="LOGO" />
             </div>
-            <div className="search_bar_wrapper">
-                <input className="search_bar_input" type="text" min="3" title="Barra de pesquisa" placeholder="Faça sua pesquisa" />
-                <PiMagnifyingGlassBold className="search_bar_icon" />
+            <div className="header_searchbar">
+                <div>
+                    <input className="search_bar_input" type="text" min="3" title="Barra de pesquisa" placeholder="Faça sua pesquisa" />
+                    <PiMagnifyingGlassBold className="icon" />
+                </div>
+            </div>
+            <div className="header_element-menu">
+                <RxHamburgerMenu className="icon" />
             </div>
             <div className="header_element">
-                <LuHeart className="header_element_icon" />
+                <LuHeart className="icon" />
                 <p className="header_element_title">LISTA DE DESEJOS</p>
             </div>
             <div className="header_element">
-                <CgProfile className="header_element_icon" />
+                <CgProfile className="icon" />
                 <p className="header_element_title">PERFIL</p>
             </div>
-            <div className="header_element_car">
-                <MdOutlineLocalGroceryStore className="header_element_icon" />
-                <p className="header_element_car_title">CARRINHO</p>
+            <div className="header_element-car">
+                <MdOutlineLocalGroceryStore className="icon" />
+                <p>CARRINHO</p>
             </div>
         </div>
     )
